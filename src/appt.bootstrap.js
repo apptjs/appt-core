6a551a14e3733f61413ff74457c9e4f38df43b9d
Rodrigo Brabo <brab0@MacBook-Pro-de-Rodrigo.local>
2018-09-25 23:00:05 -0300

    fix issue when application has more than one Bootstrap entry

--- src/appt.bootstrap.js
+++ src/appt.bootstrap.js
@@ -1,19 +1,25 @@
 import apptEcosystem from './appt.ecosystem'
 
+var booted = false;
+
 class Bootstrap {
    constructor(){}
 
    module(mainModule) {
-      
-      const config = require(process.cwd() + '/appt.json');
-        
-      apptEcosystem.bootFiles(config.include, config.exclude);
-      
-      const ApptModule = typeof mainModule === 'string' 
-         ? apptEcosystem.getEntity(mainModule, 'your application\'s entrypoint')
-         : mainModule;
-      
-      new ApptModule();
+      /** fix multibootable applications */
+      if(!booted){
+         booted = true
+         
+         const config = require(process.cwd() + '/appt.json');
+         
+         apptEcosystem.bootFiles(config.include, config.exclude);
+         
+         const ApptModule = typeof mainModule === 'string' 
+            ? apptEcosystem.getEntity(mainModule, 'your application\'s entrypoint')
+            : mainModule;
+         
+         new ApptModule();      
+      }
    }
 }
 
