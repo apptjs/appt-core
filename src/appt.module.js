e6455e59c8b2cb27510332b58c81a9d0a3eb488f
Rodrigo Brabo <brab0@MacBook-Pro-de-Rodrigo.local>
2018-09-27 20:07:09 -0300

    update version

--- src/appt.module.js
+++ src/appt.module.js
@@ -105,7 +105,7 @@ export class ApptModuleEntity {
 
 export default function ApptModule(decoratorArgs)  {
       return function decorator(Target) {
-            return function(args){                  
+            return function(args){
             const apptModuleEntity = new ApptModuleEntity(Target.name, decoratorArgs);
             
             return apptModuleEntity
