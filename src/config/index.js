8bc2b0270f8a0e9bcfd105042819352725e07a61
Rodrigo Brabo <brabo.rodrigo@gmail.com>
2018-11-03 03:41:49 -0300

    fix config when its export or module.exports

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,6 @@ import apptConfig from '../appt.config';
 
 apptConfig.set(process.cwd(), process.env.NODE_ENV);
 
-module.exports = apptConfig.getConfig().default;
+const config = apptConfig.getConfig();
+
+module.exports = config.default || config;
